cb0a84bda41940aaa5f9d690e14e1371bf8ce029
Oleksandr Savchenko <savchenkoo@mydigicode.com>
2023-12-07 21:23:22 +0200

    crud mysql

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,6 @@ app.get('/', (req, res) => {
 })
 
 app.post('/v1/user', (req, res) => {
-
     const { email, name } = req.body;
 
     if (emailValidator(email) && name.length >= 2) {
@@ -32,6 +31,51 @@ app.post('/v1/user', (req, res) => {
         res.send('something went wrong, check request body')
     }
 
+});
+
+app.put('/v1/user/:id', (req, res) => {
+    const id = req.params.id;
+    const { email, name } = req.body;
+
+    if (id || id === 0) {
+        const sql = "UPDATE Users SET email = ?, name = ? WHERE id = ?";
+        const data = [email, name, id]
+
+        mysql.query(sql, data, (err, result) => {
+            console.log(result)
+        })
+        res.send('put request was successful')
+    } else {
+        res.send('something went wrong')
+    }
+})
+
+app.get('/v1/user/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id || id === 0) {
+        const sql = `SELECT * FROM Users WHERE id = ${id}`;
+
+        mysql.query(sql, (err, result) => {
+            console.log(result)
+        })
+        res.send('get by id request was successful')
+    } else {
+        res.send('something went wrong')
+    }
+});
+
+app.delete('/v1/user/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM Users WHERE id = ${id}`;
+
+    mysql.query(sql, (err, result) => {
+        if (err) {
+            console.log("error: ", err);
+        }
+        console.log("deleted user with id: ", id);
+    })
+    res.send('delete by id request was successful')
 })
 
 app.listen(5000, () => {
