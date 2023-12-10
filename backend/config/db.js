1babecfd697e5a8a1262e6081548ab38a95c2a13
Oleksandr Savchenko <savchenkoo@mydigicode.com>
2023-12-10 19:12:21 +0200

    database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql2';
-import dbConfig from './db.config.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const connection = mysql.createConnection({
-  host: dbConfig.host,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((error) => {
